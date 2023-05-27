72af6eaadfeeb5d5c5d2405685f213b9e0929e2a
ftm <99824014+ftmakdeniz@users.noreply.github.com>
2023-05-27 23:54:43 +0300

    Add files via upload

---
+++ rastgele-sayı/rastgele-sayı/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 101);
+            int attempts = 5;
+
+            Console.WriteLine("The computer has picked a number between 1 and 100. You have 5 attempts to guess it.");
+
+            while (attempts > 0)
+            {
+                Console.Write("Enter your guess: ");
+                int guess;
+                if (int.TryParse(Console.ReadLine(), out guess))
+                {
+                    if (guess == randomNumber)
+                    {
+                        Console.WriteLine("Congratulations! You have guessed the correct number.");
+                        return;
+                    }
+                    else if (guess < randomNumber)
+                    {
+                        Console.WriteLine("Go higher.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Go lower.");
+                    }
+
+                    attempts--;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please try again.");
+                }
+            }
+
+            Console.WriteLine("Sorry, you have run out of attempts. The number was: " + randomNumber);
+        }
+    }
+}
+//ftm
+//ftm
+//ftm
